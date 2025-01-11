078957178bb338580c9acd1d31cbf0ba8b6e6840
Ahmed Fawzy <ahmadfawzy1399@gmail.com>
2025-01-11 23:08:03 +0200

    Game Board Update

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("/FXML/ProfilePageFXML.fxml"));        
+    Parent root = FXMLLoader.load(getClass().getResource("/FXML/GameBoard.fxml"));        
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/Resources/Pages.css").toExternalForm());
         stage.setScene(scene);
