7275ea9b5f5894d8e8d6fe7f093b010cfe3c6f46
Yara <yarramohammed02@gmail.com>
2025-01-18 02:03:48 +0200

    add JSON

--- src/Utils/Encapsulator.java
+++ src/Utils/Encapsulator.java
@@ -1,17 +1,30 @@
 package Utils;
 
+import org.json.JSONObject;
+
 public class Encapsulator {
+    public static String encapsulateSignIn(String username, String password) {
+        JSONObject json = new JSONObject();
+        json.put("requestType", "SIGN_IN");
+        json.put("username", username);
+        json.put("password", password);
+        return json.toString();
+    }
+
+    public static String encapsulateSignUp(String username, String email, String password) {
+        JSONObject json = new JSONObject();
+        json.put("requestType", "SIGN_UP");
+        json.put("username", username);
+        json.put("password", password);
+        json.put("email", email);
+        return json.toString();
+    }
     
-    public static String encapsulate(String requestType, String ...args){
-        String message = requestType;
-        message = message.concat(":");
-        for(int i=0;i<args.length;i++){
-            message = message.concat(args[i]);
-            if(i<args.length-1){
-                message=message.concat(":");
-                
-            }
-        }
-        return message;
-    }  
+    public static String encapsulateSignOut(int PlayerID) {
+        JSONObject json = new JSONObject();
+        json.put("requestType", "SIGN_OUT");
+        json.put("Player_ID", PlayerID);
+        return json.toString();
+    }
 }
+
