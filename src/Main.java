77013be57c24f7508df79f07a890befd77c6ab2c
ahmadFawzy13 <ahmadfawzy1399@gmail.com>
2025-01-18 15:49:07 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         
-        Parent root = FXMLLoader.load(getClass().getResource("/FXML/GameBoard.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/FXML/HomePage.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/Resources/Pages.css").toExternalForm());
         stage.setScene(scene);
